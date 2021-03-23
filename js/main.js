01e02107c00796b1facd751404a87d936a0c0fbd
GennadyVenidiktov <wenter3@outlook.com>
2021-03-23 20:57:20 +0500

    Модальное окно + плавная прокрутка
    Плавная проктрутка стандартной функцией js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,45 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+//cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+
+const openModal = (event) => {
+	modalCart.classList.add('show');
+	document.addEventListener('keydown', escapeHandler);
+}
+
+const closeModal = () => {
+	modalCart.classList.remove('show');
+	document.removeEventListener('keydown', escapeHandler);
+}
+
+const escapeHandler = event => {
+	if (event.code === 'Escape') closeModal();
+}
+
+modalCart.addEventListener('click', (e) => {
+	if (e.target.classList.contains('modal-close') || !e.target.closest('.modal')) {
+		closeModal();
+	}
+})
+
+buttonCart.addEventListener('click', openModal);
+
+//scroll smoothly
+(() => {
+	const scrollLink = document.querySelectorAll('a.scroll-link ');
+
+	for (let i = 0; i < scrollLink.length; i++) {
+		scrollLink[i].addEventListener('click', (event) => {
+			event.preventDefault();
+			const id = scrollLink[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	}
+})()
